ecd38eb346ffb4f31fe007753cb28b5acfda0749
Roman Culjak <romanculjak.dev@gmail.com>
2023-02-27 12:22:10 +0100

    cleanup of unused code

--- src/index.ts
+++ src/index.ts
@@ -20,28 +20,6 @@ const db = admin.firestore();
 
 //routes
 
-// app.get("/api/createmake", (req: Request, res: Response)=>{
-//     return res.status(200).send("Hello, cloud functions are working as expected!");
-// })
-
-// app.get("/api/makes",async (req: Request, res: Response) => {
-//     try{
-//         const result = await (await db.collection('vehiclemake').get()).docs;
-//         // const data = result.map((item)=> item.data());
-//         const data = result.map(item=>{
-
-//             return (
-//                 {id:item.id, ...item.data()}
-//             )
-//         })
-        
-
-//         res.status(200).send({status:'success', msg:'data saved', docs:data})
-//     } catch{
-//         res.status(500).send({status:'failed', msg:'something failed'})
-//     }
-// })
-
 
 app.post("/api/createmake",async (req: Request, res: Response) => {
     
@@ -129,27 +107,9 @@ app.put("/api/updatemodel",async (req: Request, res: Response) => {
     }
 })
 
-// app.get("/api/makes/sort", async (req : Request, res: Response) => {
-    
-    
 
-//     try{
-//     const result = await (await db.collection('vehiclemake').where('country', '==', req.query.country).limit(parseInt(req.query.limit?.toString()!)).get()).docs;
 
-//     const data = result.map(item=>{
 
-//         return (
-//             {id:item.id, ...item.data()}
-//         )
-//     })
-    
-
-//     res.status(200).send({status:'success', msg:'data saved', docs:data})
-// }catch(e){
-//     res.status(500).send({status:'failed', msg:'i dont know', e})
-// }
-
-// })
 
 app.get("/api/makes/", async (req : Request, res: Response) => {
     
@@ -194,7 +154,7 @@ app.get("/api/makes/", async (req : Request, res: Response) => {
     
 
     res.status(200).send({status:'success', msg:'makes data has been fetched successfully', docs:data})
-    
+
 }catch(e){
     res.status(500).send({status:'failed', msg:'makes data has been FAILED to be retreived successfully', e})
 }
